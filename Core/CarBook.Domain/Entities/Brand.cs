24ac29804b4945110c36f9386b6d6d06ba218412
Yücel AYBEY <yucelaybey06@outlook.com>
2025-02-26 12:30:04 +0300

    entities

---
+++ Core/CarBook.Domain/Entities/Brand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Brand
+    {
+        public int BrandID { get; set; }
+        public int Name { get; set; }
+    }
+}
