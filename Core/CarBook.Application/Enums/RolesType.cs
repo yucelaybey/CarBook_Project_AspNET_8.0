cf48cca5355d1a064b66baf6a3b2127a8675d639
Yücel AYBEY <yucelaybey06@outlook.com>
2025-07-02 11:28:56 +0300

    AppUser Register Operation

---
+++ Core/CarBook.Application/Enums/RolesType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Enums
+{
+	public enum RolesType
+	{
+		Admin = 1,
+		Member = 2
+	}
+}
